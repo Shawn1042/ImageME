bbf2704baf9019783df90b4160590aa0254831c8
Shawn1042 <shawnholmes9921@gmail.com>
2025-01-14 13:01:21 -0500

    navbar and installed react router

---
+++ imageMe/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+// src/components/Navbar.jsx
+import React from 'react';
+
+const Navbar = ({ onNavbarClick }) => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">
+        Image<span className="navbar-title-highlight">Me</span>
+      </div>
+      <div className="navbar-buttons">
+        <button onClick={() => onNavbarClick('login')}>Login</button>
+        <button onClick={() => onNavbarClick('signup')}>Signup</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
